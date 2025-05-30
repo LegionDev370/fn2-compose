fe38f890071dc9f382050eebd8791c0e993e71ed
LegionDev370 <llegion630@gmail.com>
2025-05-30 16:58:59 +0500

    added azamatsasa

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Azamatjon!';
+    return 'Hello World Azamatjonsss!';
   }
 }
